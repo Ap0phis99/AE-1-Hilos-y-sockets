3136faf20f41af774685ba373cad9900a8bc0d3b
Rubén González Rodríguez <114941873+Ap0phis99@users.noreply.github.com>
2022-10-25 18:09:40 +0100

    Borrados antiguos directorios

--- src/users/Admin.java
+++
@@ -1,8 +0,0 @@
-package users;
-
-public class Admin extends Persona{
-
-	public Admin(String email, String pw) {
-		super(email, pw, "addbook,removebook");
-	}
-}
--- src/users/User.java
+++
@@ -1,8 +0,0 @@
-package users;
-
-public class User extends Persona{
-
-	public User (String email, String pw) {
-		super(email, pw, "borrowbytitle,borrowbyisbn,backbook");
-	}
-}
